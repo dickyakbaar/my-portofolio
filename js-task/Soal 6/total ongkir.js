ac9bda254d2c4e3fb22a1c7eb529fe4225f75d6f
Dicky Akbar Hega Putra <101337709+dickyakbaar@users.noreply.github.com>
2022-11-22 09:11:59 +0700

    Add files via upload

---
+++ js-task/Soal 6/total ongkir.js
@@ -0,0 +1,12 @@
+let jarak = 4
+let ongkir1 = 5000
+let ongkir2 = ongkir1
+ongkir2 = ongkir2 + 3000
+
+if (jarak <= 2) {
+ console.log(ongkir1)
+} else if (jarak > 2){
+ console.log(ongkir2)
+} else {
+  console.log('Error')
+}
